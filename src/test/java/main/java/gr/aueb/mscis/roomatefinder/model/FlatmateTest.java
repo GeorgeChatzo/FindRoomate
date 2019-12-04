2bdd947f72deaea736b3cdc4817fefa7699a97b6
irinil <pazi.lyg@gmail.com>
2019-12-04 18:07:25 +0200

    new Package model

---
+++ src/test/java/main/java/gr/aueb/mscis/roomatefinder/model/FlatmateTest.java
@@ -0,0 +1,14 @@
+package main.java.gr.aueb.mscis.roomatefinder.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlatmateTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
