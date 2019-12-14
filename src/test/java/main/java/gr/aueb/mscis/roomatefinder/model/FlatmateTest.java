12bd8f3576188d3ae0222cd7974a0fc73350bf98
Georgios Chatzopoulos <giorgoschatzopoulos7@gmail.com>
2019-12-14 20:16:42 +0200

    flatmate update

--- src/test/java/main/java/gr/aueb/mscis/roomatefinder/model/FlatmateTest.java
+++ src/test/java/main/java/gr/aueb/mscis/roomatefinder/model/FlatmateTest.java
@@ -2,6 +2,7 @@ package main.java.gr.aueb.mscis.roomatefinder.model;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -42,6 +43,27 @@ public class FlatmateTest {
 		habits.add("gym lover");
 		
 		return habits;
+	}
+	
+	@Test
+	public void testFlatmateConstructorSuper() {
+		
+		Set<Double> rating = new HashSet<Double>();
+		EmailAddress email = new EmailAddress("g");
+		CellNumber number = new CellNumber("2");
+		Flatmate fl = new Flatmate("GChatz","qwerty123",email,number,"George","Chatzopoulos"
+				,22,rating);
+		fl.setEmail(email);
+		fl.setPhoneNumber(number);
+		fl.setRating(rating);
+		assertTrue(fl.getUsername().equals("GChatz"));
+		assertTrue(fl.getPassword().equals("qwerty123"));
+		assertTrue(fl.getEmail().equals(email));
+		assertTrue(fl.getPhoneNumber().equals(number));
+		assertTrue(fl.getAge()==22);
+		assertTrue(fl.getRating().equals(rating));
+
+
 	}
 	
 	
@@ -99,6 +121,7 @@ public class FlatmateTest {
 		flatmate.setRating(rating);
 		flatmate.setHabits(habits);
 		flatmate.setWorkSchedule("Monday-Saturday");
+		
 
 		assertTrue(flatmate.getUsername().equals("valkerian"));
 		assertTrue(flatmate.getPassword().equals("123"));
@@ -114,148 +137,13 @@ public class FlatmateTest {
 		assertTrue(flatmate.isIncomingGuests()==false);
 		assertTrue(flatmate.getRating().equals(rating));
 		assertTrue(flatmate.getPhoneNumber().equals(number));
+		assertTrue(flatmate.getHabits().equals(habits));
+		
 
 	}
 	
 	
-//	@Test
-//	public void testName() {
-//		
-//		Flatmate flatmate = new Flatmate();	
-//		flatmate.setName("Dildo");
-//		assertTrue(flatmate.getName().equals("Dildo"));
-//	
-//	}
-//	
-//	@Test
-//	public void testSurname() {
-//		
-//		Flatmate flatmate = new Flatmate();	
-//		flatmate.setSurname("Baggins");
-//		assertTrue(flatmate.getSurname().equals("Baggins"));
-//		
-//	}
-//	
-//	@Test
-//	public void testDescription() {
-//		
-//		Flatmate flatmate = new Flatmate();	
-//		flatmate.setDescription("Enoikiazetai spiti");
-//		assertTrue(flatmate.getDescription().equals("Enoikiazetai spiti"));
-//		
-//	}
-//	
-//	@Test
-//	public void testGender() {
-//		
-//		Flatmate flatmate = new Flatmate();	
-//		flatmate.setGender("Male");
-//		assertTrue(flatmate.getGender().equals("Male"));
-//		
-//	}
-//	
-//	@Test
-//	public void testProfessionCaseStudent() {
-//		
-//		status profession = status.STUDENT;
-//		Flatmate flatmate = new Flatmate();	
-//		flatmate.setProfession(profession);
-//		assertTrue(flatmate.getProfession().equals(profession));
-//		
-//	}
-//	
-//	@Test
-//	public void testProfessionCaseEmployee() {
-//		
-//		status profession = status.EMPLOYEE;
-//		Flatmate flatmate = new Flatmate();	
-//		flatmate.setProfession(profession);
-//		assertTrue(flatmate.getProfession().equals(profession));
-//		
-//	}
-//	
-//	@Test
-//	public void testProfessionCaseUnemployed() {
-//		
-//		status profession = status.UNEMPLOYED;
-//		Flatmate flatmate = new Flatmate();	
-//		flatmate.setProfession(profession);
-//		assertTrue(flatmate.getProfession().equals(profession));
-//		
-//	}
-//	
-//	@Test
-//	public void testPets() {
-//		
-//		Flatmate flatmate = new Flatmate();	
-//		flatmate.setPets(true);
-//		assertTrue(flatmate.isPets()==true);
-//		
-//	}
-//	
-//	@Test
-//	public void testHabitsAddition() {
-//		
-//		Flatmate flatmate = new Flatmate();	
-//		Set<String> habits = new HashSet<String>();
-//		habits.add("smoker");
-//		habits.add("gym lover");
-//		flatmate.setHabits(habits);
-//		assertTrue(flatmate.getHabits().equals(habits));
-//		
-//	}
-//	
-//	@Test
-//	public void testHabitsRemove() {
-//		
-//		Flatmate flatmate = new Flatmate();	
-//		Set<String> habits = new HashSet<String>();
-//		habits.add("smoker");
-//		habits.add("gym lover");
-//		flatmate.setHabits(habits);
-//		habits.remove("smoker");
-//		assertTrue(flatmate.getHabits().equals(habits));
-//		
-//	}
-//	
-//	@Test
-//	public void testWorkSchedule() {
-//		
-//		Flatmate flatmate = new Flatmate();	
-//		String workSchedule = "Everyday all day";
-//		flatmate.setWorkSchedule(workSchedule);
-//		assertTrue(flatmate.getWorkSchedule().equals(workSchedule));
-//		
-//	}
-//	
-//	@Test
-//	public void testIncomingGuests() {
-//		
-//		Flatmate flatmate = new Flatmate();	
-//		flatmate.setIncomingGuests(true);
-//		assertTrue(flatmate.isIncomingGuests()==true);
-//		
-//	}
-//	
-//	@Test
-//	public void testEmail() {
-//		
-//		EmailAddress email = new EmailAddress("giorgos@gmail.com");
-//		Flatmate flatmate = new Flatmate();
-//		flatmate.setEmail(email);
-//		assertTrue(flatmate.getEmail().equals(email));
-//		
-//	}
-//	
-//	@Test
-//	public void testPhone() {
-//		
-//		CellNumber phone = new CellNumber("210123456");
-//		Flatmate flatmate = new Flatmate();
-//		flatmate.setPhoneNumber(phone);
-//		assertTrue(flatmate.getPhoneNumber().equals(phone));
-//		
-//	}
+
 	
 	@Test
 	public void testAddCohabitRequest() {
@@ -314,10 +202,460 @@ public class FlatmateTest {
 		
 	}
 	
+	@Test
+	public void testRequestNotContains() {
+		
+		Flatmate flatmate = new Flatmate();
+		HouseAd ad = new HouseAd();
+		CohabitRequest corequest = new CohabitRequest();
+		corequest = flatmate.request(ad);
+		flatmate.cancelRequest(corequest);
+		assertTrue(flatmate.getCohabitRequest().size()==0);
+
+		
+	}
+	
+	@Test
+	public void testAddCohabitNull() {
+		
+		CohabitRequest corequest = null;
+		flatmate.addCohabitRequest(corequest);
+		assertTrue(flatmate.getCohabitRequest().size()==0);
+		
+	}
+	
+	@Test
+	public void testRemoveCohabitNull() {
+		
+		CohabitRequest corequest = null;
+		flatmate.addCohabitRequest(corequest);
+		flatmate.removeCohabitRequest(corequest);
+		assertTrue(flatmate.getCohabitRequest().size()==0);
+		
+	}
+	
+	@Test
+	public void testValidateFields1() {
+		
+		EmailAddress email = new EmailAddress("g");
+		CellNumber number = new CellNumber("2");
+		flatmate.setPhoneNumber(number);
+		flatmate.setEmail(email);
+		assertTrue(flatmate.validateFields()==true);
+		
+	}
+	
+	@Test
+	public void testValidateFields2() {
+		
+		EmailAddress email = null;
+		CellNumber number = new CellNumber("2");
+		flatmate.setPhoneNumber(number);
+		flatmate.setEmail(email);
+		assertTrue(flatmate.validateFields()==false);
+		
+	}
+	
+	@Test
+	public void testValidateFields3() {
+		
+		EmailAddress email = new EmailAddress("g");
+		CellNumber number = null;
+		flatmate.setPhoneNumber(number);
+		flatmate.setEmail(email);
+		assertTrue(flatmate.validateFields()==false);
+		
+	}
+	
+	@Test
+	public void testObjects() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		assertTrue(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testObject() {
+		
+		assertTrue(flatmate.equals(flatmate));
+		
+	}
+	
+	@Test
+	public void testOtherObject() {
+		
+		Flatmate fl1 = new Flatmate();
+		HouseAd ad1 = new HouseAd();
+		assertFalse(fl1.equals(ad1));
+		
+	}
+	
+	@Test
+	public void testNullObject() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = null;
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testCohabitReq1() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		CohabitRequest req1 = new CohabitRequest();
+		CohabitRequest req2 = new CohabitRequest();
+		fl1.addCohabitRequest(req1);
+		fl2.addCohabitRequest(req2);
+		assertFalse(flatmate.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testCohabitReq2() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		CohabitRequest req1 = null;
+		CohabitRequest req2 = new CohabitRequest();
+		fl1.addCohabitRequest(req1);
+		fl2.addCohabitRequest(req2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testCohabitReq3() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		CohabitRequest req2 = new CohabitRequest();
+		fl1.addCohabitRequest(req2);
+		fl2.addCohabitRequest(req2);
+		assertTrue(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testCohabitReq4() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		HouseAd ad = new HouseAd();
+		Date date = new Date(System.currentTimeMillis());
+		CohabitRequest req2 = new CohabitRequest(flatmate,true,date,ad);
+		CohabitRequest req1 = null;
+		fl1.addCohabitRequest(null);
+		fl2.addCohabitRequest(req2);
+		assertFalse(fl1.getCohabitRequest().equals(fl2.getCohabitRequest()));
+		
+	}
+	
+	@Test
+	public void testDescription1() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		fl1.setDescription("empty2");
+		fl2.setDescription("empty1");
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testDescription2() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		fl1.setDescription(null);
+		fl2.setDescription("empty1");
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testDescription3() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		fl1.setDescription("empty1");
+		fl2.setDescription("empty1");
+		assertTrue(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testEmail1() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		EmailAddress email1 = new EmailAddress("gio1@aueb.gr");
+		EmailAddress email2 = new EmailAddress("gio2@aueb.gr");
+		fl1.setEmail(email1);
+		fl2.setEmail(email2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testEmail2() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		EmailAddress email1 = new EmailAddress("gio@aueb.gr");
+		EmailAddress email2 = new EmailAddress("gio@aueb.gr");
+		fl1.setEmail(email1);
+		fl2.setEmail(email2);
+		assertTrue(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testEmail3() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		EmailAddress email1 = null;
+		EmailAddress email2 = new EmailAddress("gio@aueb.gr");
+		fl1.setEmail(email1);
+		fl2.setEmail(email2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
 	
 	
+	@Test
+	public void testGender1() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		String gender1 = "Male";
+		String gender2 = "Female";
+		fl1.setGender(gender1);
+		fl2.setGender(gender2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
 	
+	@Test
+	public void testGender2() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		String gender1 = "Male";
+		String gender2 = "Male";
+		fl1.setGender(gender1);
+		fl2.setGender(gender2);
+		assertTrue(fl1.equals(fl2));
+		
+	}
 	
+	@Test
+	public void testGender3() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		String gender1 = null;
+		String gender2 = "Male";
+		fl1.setGender(gender1);
+		fl2.setGender(gender2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testHabits1() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		Set<String> habits1 = new HashSet<String>();
+		Set<String> habits2 = new HashSet<String>();
+		habits1.add("smoker");
+		fl1.setHabits(habits1);
+		fl2.setHabits(habits2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testHabits2() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		Set<String> habits1 = new HashSet<String>();
+		Set<String> habits2 = new HashSet<String>();
+		fl1.setHabits(habits1);
+		fl2.setHabits(habits1);
+		assertTrue(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testHabits3() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		Set<String> habits1 = null;
+		Set<String> habits2 = new HashSet<String>();
+		fl1.setHabits(habits1);
+		fl2.setHabits(habits2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testIncomingGuests() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		fl1.setIncomingGuests(true);
+		fl2.setIncomingGuests(false);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testPets() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		fl1.setPets(true);
+		fl2.setPets(false);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testPhone1() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		CellNumber phone1 = new CellNumber("210123456");
+		CellNumber phone2 = new CellNumber("210123457");
+		fl1.setPhoneNumber(phone1);
+		fl2.setPhoneNumber(phone2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testPhone2() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		CellNumber phone1 = new CellNumber("210123456");
+		CellNumber phone2 = new CellNumber("210123456");
+		fl1.setPhoneNumber(phone1);
+		fl2.setPhoneNumber(phone2);
+		assertTrue(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testPhone3() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		CellNumber phone1 = null;
+		CellNumber phone2 = new CellNumber("210123456");
+		fl1.setPhoneNumber(phone1);
+		fl2.setPhoneNumber(phone2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testProfession() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		fl1.setProfession(status.STUDENT);
+		fl2.setProfession(status.EMPLOYEE);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testRating1() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		Set<Double> rating1 = new HashSet<Double>();
+		Set<Double> rating2 = new HashSet<Double>();
+		rating1.add(9.0);
+		fl1.setRating(rating1);
+		fl2.setRating(rating2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testRating2() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		Set<Double> rating1 = new HashSet<Double>();
+		Set<Double> rating2 = new HashSet<Double>();
+		fl1.setRating(rating1);
+		fl2.setRating(rating2);
+		assertTrue(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testRating3() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		Set<Double> rating1 = null;
+		Set<Double> rating2 = new HashSet<Double>();
+		fl1.setRating(rating1);
+		fl2.setRating(rating2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testScedule1() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		String schedule1 = "Monday-Friday";
+		String schedule2 = "Monday-Saturday";
+		fl1.setWorkSchedule(schedule1);
+		fl2.setWorkSchedule(schedule2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testScedule2() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		String schedule1 = "Monday-Friday";
+		String schedule2 = "Monday-Friday";
+		fl1.setWorkSchedule(schedule1);
+		fl2.setWorkSchedule(schedule2);
+		assertTrue(fl1.equals(fl2));
+		
+	}
+	
+	@Test
+	public void testScedule3() {
+		
+		Flatmate fl1 = new Flatmate();
+		Flatmate fl2 = new Flatmate();
+		String schedule1 = null;
+		String schedule2 = "Monday-Friday";
+		fl1.setWorkSchedule(schedule1);
+		fl2.setWorkSchedule(schedule2);
+		assertFalse(fl1.equals(fl2));
+		
+	}
 	
 
 }
